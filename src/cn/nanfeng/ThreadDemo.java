bfb22dd43c3a95c73c53ff398e0ba450945ea52d
yangnanfeng <1751985863@qq.com>
2020-04-26 21:51:24 +0800

    同步方法是锁的this

--- src/cn/nanfeng/ThreadDemo.java
+++ src/cn/nanfeng/ThreadDemo.java
@@ -1,53 +1,54 @@
 package cn.nanfeng;
 
 public class ThreadDemo {
-    public static void main(String[] args) {
-        long startime=System.currentTimeMillis();
+    public static void main(String[] args) throws InterruptedException {
         ThreadTrain2 threadTrain = new ThreadTrain2();
-        Thread thread = new Thread(threadTrain);
         Thread thread1 = new Thread(threadTrain);
-        thread.start();
+        Thread thread2 = new Thread(threadTrain);
         thread1.start();
-        long endtime=System.currentTimeMillis();
-        System.out.println("程序运行时间为"+(endtime-startime));
+        Thread.sleep(100);
+        threadTrain.flag=false;
+        thread2.start();
+
+
 
 
     }
 }
 
-class ThreadTrain implements Runnable{
-
-    private int count=100;
-    private Object object=new Object();
-    @Override
-    public void run() {
-
-        while (count>0){
-            synchronized (object){
-
-                if (count>0){
-                    System.out.println(Thread.currentThread()+"出售第"+(100-count+1)+"张票");
-                    count--;
-                }
-            }
-        }
-
-    }
-}
 class ThreadTrain2 implements Runnable{
 
     private int count=100;
     private Object object=new Object();
-    @Override
-    public void run() {
 
-        while (count>0){
-            show();
+    public boolean flag=true;
+    public void run() {
+        if (flag){
+            while (count>0){
+                synchronized (object){
+
+                    if (count>0){
+                        try {
+                            Thread.sleep(40);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                        System.out.println(Thread.currentThread()+"出售第"+(100-count+1)+"张票");
+                        count--;
+                    }
+                }
+            }
+        }
+
+        else {
+            while (count>0){
+                show();
+            }
         }
 
     }
     public  void show(){
-        synchronized(this) {
+        synchronized(object) {
             if (count > 0) {
                 try {
                     Thread.sleep(40);
