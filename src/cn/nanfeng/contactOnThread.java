fd0639bd17a2c2e4825038c3078df47bdd14166b
yangnanfeng <1751985863@qq.com>
2020-05-02 22:22:33 +0800

    生产者消费者，线程通信lock版

--- src/cn/nanfeng/contactOnThread.java
+++ src/cn/nanfeng/contactOnThread.java
@@ -1,5 +1,9 @@
 package cn.nanfeng;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  *使用同步代码块线程不安全
  */
@@ -21,6 +25,8 @@ class Res{
     public String name;
     public String sex;
     public boolean flag=false;//如果flag为false表示不能读，只能写
+    public Lock lock=new ReentrantLock();
+    public Condition condition=lock.newCondition();
 
     @Override
     public String toString() {
@@ -45,17 +51,12 @@ class inputThread extends Thread{
     public void run() {
         int count=0;
         while (true){
-            synchronized (res){//加锁
-                if (res.flag) {
-                    // 当前线程等待。wait();类似于 sleep 可以让当前线程，从运行状态变为休眠状态 wait
-                    // 使用在多线程之间同步 和synchronized 一起使用
-                    // wait可以释放锁 ,sleep 不能释放锁。
-                    try {
-                        res.wait();
-                    } catch (Exception e) {
-                        // TODO: handle exception
-                    }
+            try {
+                res.lock.lock();
+                if (res.flag){
+                        res.condition.await();
                 }
+
                 if (count == 0) {
                     res.name = "余胜军";
                     res.sex = "男";
@@ -67,7 +68,13 @@ class inputThread extends Thread{
                 count = (count + 1) % 2;
                 res.flag = true;
                 // 和wait 一起使用 唤醒拧一个线程  唤醒从阻塞状态变为运行状态
-                res.notify();
+                res.condition.signal();
+            }catch (Exception e){
+
+            }
+            finally {
+                res.lock.unlock();
+            }
 
 
 
@@ -76,7 +83,7 @@ class inputThread extends Thread{
 
 
 
-        }
+
     }
 }
 
@@ -94,19 +101,24 @@ class outThread extends Thread{
     public void run() {
         int count=0;
         while (true){
-            synchronized (res){//加锁2
-                if (!res.flag) {
-                    try {
-                        res.wait();
+            try {
+                res.lock.lock();
+                if (!res.flag){
 
-                    } catch (Exception e) {
-                        // TODO: handle exception
-                    }
+                    res.condition.await();
+
                 }
                 System.out.println(res.name + "---" + res.sex);
                 res.flag = false;
-                res.notify();
+                res.condition.signal();
+            }catch (Exception e){
+
             }
+            finally {
+                res.lock.unlock();
+            }
+
+
 
         }
     }
