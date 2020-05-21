999d9568d2c6e64c5be445bda1d6902eecc07d58
yangnanfeng <1751985863@qq.com>
2020-05-21 22:12:31 +0800

    学校的作业打卡

--- src/cn/cqupt/doit/Book.java
+++ src/cn/cqupt/doit/Book.java
@@ -61,6 +61,7 @@ public class Book implements Comparable {
         /**/
         /**/
         /**/
+        /**/
         System.out.println("input a new Book:");
         temp=input.nextLine();
         message = temp.split(",");
